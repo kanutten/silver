ef7f1f3672cac2315870d985aee493ab92ac7340
kanutten <44906532+kanutten@users.noreply.github.com>
2025-03-18 20:23:23 +0100

    ostree

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
-
+#dnf5 install -y tmux 
+rpm-ostree install -y tmux
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
