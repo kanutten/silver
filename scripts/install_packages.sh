6ab3bd38dd0a4e0b13c302b0bbdbef569ecd2860
kanutten <44906532+kanutten@users.noreply.github.com>
2025-03-19 16:32:21 +0100

    test

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -10,33 +10,33 @@ utility=(
 )
 
 extensions=(
-  "gnome-shell-extension-just-perfection.noarch"
-  "thinkpad-battery-threshold@marcosdalvarez.org"
-  "caffeine@patapon.info"
-  "solaar-extension@sidevesh"
-  "thinkpadthermal@moonlight.drive.vk.gmail.com"
-  "status-area-horizontal-spacing@mathematical.coffee.gmail.com"
-  "weatheroclock@CleoMenezesJr.github.io"
-  "quick-settings-tweaks@qwreey"
-  "clipboard-indicator@tudmotu.com"
-  "openbar@neuromorph"
-  "extension-list@tu.berry"
-  "pinned-apps-in-appgrid@brunosilva.io"
-  #"wiggle@mechtifs"
-  "foresight@pesader.dev"
-  "tilingshell@ferrarodomenico.com"
-  "space-bar@luchrioh"
-  #"appindicatorsupport@rgcjonas.gmail.com"
-  #"apps-menu@gnome-shell-extensions.gcampax.github.com"
-  "blur-my-shell@aunetx"
-  "dash-to-dock@micxgx.gmail.com"
-  "gsconnect@andyholmes.github.io"
-  "launch-new-instance@gnome-shell-extensions.gcampax.github.com"
-  "logomenu@aryan_k"
-  "places-menu@gnome-shell-extensions.gcampax.github.com"
-  "search-light@icedman.github.com"
-  "tailscale@joaophi.github.com"
-  "user-theme@gnome-shell-extensions.gcampax.github.com"
+  "gnome-shell-extension-just-perfection.noarch",
+  "thinkpad-battery-threshold@marcosdalvarez.org",
+  "caffeine@patapon.info",
+  "solaar-extension@sidevesh",
+  "thinkpadthermal@moonlight.drive.vk.gmail.com",
+  "status-area-horizontal-spacing@mathematical.coffee.gmail.com",
+  "weatheroclock@CleoMenezesJr.github.io",
+  "quick-settings-tweaks@qwreey",
+  "clipboard-indicator@tudmotu.com",
+  "openbar@neuromorph",
+  "extension-list@tu.berry",
+  "pinned-apps-in-appgrid@brunosilva.io",
+  #"wiggle@mechtifs",
+  "foresight@pesader.dev",
+  "tilingshell@ferrarodomenico.com",
+  "space-bar@luchrioh",
+  #"appindicatorsupport@rgcjonas.gmail.com",
+  #"apps-menu@gnome-shell-extensions.gcampax.github.com",
+  "blur-my-shell@aunetx",
+  "dash-to-dock@micxgx.gmail.com",
+  "gsconnect@andyholmes.github.io",
+  "launch-new-instance@gnome-shell-extensions.gcampax.github.com",
+  "logomenu@aryan_k",
+  "places-menu@gnome-shell-extensions.gcampax.github.com",
+  "search-light@icedman.github.com",
+  "tailscale@joaophi.github.com",
+  "user-theme@gnome-shell-extensions.gcampax.github.com",
   #"window-list@gnome-shell-extensions.gcampax.github.com"
 )
 
